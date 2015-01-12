40a04748d62a4d15181225dd63583028a8f524ad
ykelvis <rad2flum@gmail.com>
2015-01-13 00:24:46 +0800

    update

---
+++ scripts/shutdown_dialog.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+ACTION=`zenity --width=90 --height=200 --list --radiolist --text="Select logout action" --title="Logout" --column "Choice" --column "Action" TRUE LockScreen FALSE Reboot FALSE Shutdown FALSE Suspend`
+
+if [ -n "${ACTION}" ];then
+  case $ACTION in
+  Shutdown)
+    zenity --question --text "Are you sure you want to shutdown?" && shutdown -h now
+    ;;
+  Reboot)
+    zenity --question --text "Are you sure you want to reboot?" && reboot
+    ;;
+  Suspend)
+    zenity --question --text "Are you sure you want to suspend?" && systemctl suspend
+    ;;
+  LockScreen)
+    i3lock -e -t -i ~/lock.png
+    # Or gnome-screensaver-command -l
+    ;;
+  esac
+fi

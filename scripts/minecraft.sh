413901180d76655a17fd8b6a9611434b4a858389
ykelvis <yk@archlinuxcn.org>
2015-02-13 16:53:38 +0800

    update

--- scripts/minecraft.sh
+++ scripts/minecraft.sh
@@ -1,3 +1,3 @@
-/bin/bash
-
+#!/bin/bash
+export JAVA_HOME=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home
 java -DsocksProxyHost=127.0.0.1 -DsocksProxyPort=1080 -jar /Applications/Minecraft.app/Contents/Resources/Java/Bootstrap.jar

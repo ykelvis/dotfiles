ba6263a21d02214ee27df96d5ace6c68d057b948
ykelvis <yk@archlinuxcn.org>
2016-01-06 23:01:16 +0800

    update

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,8 +3,8 @@
 compile_it(){
     cd $arg;
     echo "$arg building begin"
-    extra-x86_64-build&>/dev/null||archlinuxcn-x86_64-build&>/dev/null||echo "$arg build failed";
-    extra-i686-build&>/dev/null||archlinuxcn-i686-build&>/dev/null||echo "$arg build failed";
+    extra-x86_64-build -c -r ~/build/&>/dev/null||archlinuxcn-x86_64-build -c -r ~/build/&>/dev/null||echo "$arg build failed";
+    extra-i686-build -c -r ~/build/&>/dev/null||archlinuxcn-i686-build -c -r ~/build/&>/dev/null||echo "$arg build failed";
     mv *pkg.tar.xz ~/repo&&echo "$arg DONE"||echo "$arg failed";
     cd -;
 }

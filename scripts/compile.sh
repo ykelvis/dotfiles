2645681b9d9506e8af97e016d7c11f5a541c3c69
ykelvis <yk@archlinuxcn.org>
2016-02-14 18:31:10 +0800

    update

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,11 +5,11 @@ compile_it(){
     s1="";
     s2="";
     cd $arg;
-    echo "$arg building begin";
+    echo -e "\033[31m$arg building begin\033[0m";
     archlinuxcn-x86_64-build -c -r ~/build||s1=1;
     archlinuxcn-i686-build -c -r ~/build||s2=1;
-    [[ ! -z $s1 ]]&&echo "s1 failed";
-    [[ ! -z $s2 ]]&&echo "s2 failed";
+    [[ ! -z $s1 ]]&&echo -e "\033[31ms1 failed\033[0m";
+    [[ ! -z $s2 ]]&&echo -e "\033[31ms2 failed\033[0m";
     mv *pkg.tar.xz ~/repo;
     )
 }

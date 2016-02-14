be9d7d7c33a0a9f888794963d620df51ec6f2218
ykelvis <yk@archlinuxcn.org>
2016-02-14 18:18:25 +0800

    update

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 compile_it(){
+    (
+    s1="";
+    s2="";
     cd $arg;
-    echo "$arg building begin"
-    extra-x86_64-build -c -r ~/build/&>/dev/null||archlinuxcn-x86_64-build -c -r ~/build/&>/dev/null||echo "$arg build failed";
-    extra-i686-build -c -r ~/build/&>/dev/null||archlinuxcn-i686-build -c -r ~/build/&>/dev/null||echo "$arg build failed";
-    mv *pkg.tar.xz ~/repo&&echo "$arg DONE"||echo "$arg failed";
-    cd -;
+    echo "$arg building begin";
+    archlinuxcn-x86_64-build -c -r ~/build||s1=1;
+    archlinuxcn-i686-build -c -r ~/build||s2=1;
+    [[ ! -z $s1 ]]&&echo "s1 failed";
+    [[ ! -z $s2 ]]&&echo "s2 failed";
+    mv *pkg.tar.xz ~/repo;
+    )
 }
 
 for arg in "$@"

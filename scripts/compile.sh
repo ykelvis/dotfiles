accc4ea0d038ae97f7c8e7f0e8be928ca992e7c1
ykelvis <yk@archlinuxcn.org>
2016-02-15 01:26:20 +0800

    update

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -8,8 +8,8 @@ compile_it(){
     echo -e "\033[31m$1 building begin\033[0m";
     archlinuxcn-x86_64-build -c -r ~/build||s1=1;
     archlinuxcn-i686-build -c -r ~/build||s2=1;
-    [[ ! -z $s1 ]]&&echo -e "\033[31m$1 s1 failed\033[0m";
-    [[ ! -z $s2 ]]&&echo -e "\033[31m$1 s2 failed\033[0m";
+    [[ ! -z $s1 ]]&&echo -e "\033[31m$1 x86_64 failed\033[0m";
+    [[ ! -z $s2 ]]&&echo -e "\033[31m$1 i686 failed\033[0m";
     mv *pkg.tar.xz ~/repo;
     )
 }

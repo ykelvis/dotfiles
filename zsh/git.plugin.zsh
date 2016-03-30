0364570a3c43db129483fbd47decacced25b5a51
yk <yk@archlinuxcn.org>
2016-03-31 03:00:13 +0800

    update git.plugin.zsh

--- zsh/git.plugin.zsh
+++ zsh/git.plugin.zsh
@@ -152,8 +152,8 @@ alias glgg='git log --graph --color'
 alias glgga='git log --graph --decorate --all'
 alias glgm='git log --graph --max-count=10'
 alias glo='git log --oneline --decorate --color'
-alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias glola="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --abbrev-commit --all"
+alias glol="git log --graph --pretty=format:'%Cred%h%Creset %Cgreen<%ai> %C(bold blue)[%an]%Creset -%C(yellow)%d%Creset %s' --abbrev-commit"
+alias glola="git log --graph --pretty=format:'%Cred%h%Creset %Cgreen<%ai> %C(bold blue)[%an]%Creset -%C(yellow)%d%Creset %s' --abbrev-commit --all"
 alias glog='git log --oneline --decorate --color --graph'
 alias glp="_git_log_prettily"
 compdef _git glp=git-log

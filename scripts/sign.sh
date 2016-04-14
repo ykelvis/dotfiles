4342821a1909be09008fd5b20d7ff81aef7da437
yk <yk@archlinuxcn.org>
2016-04-14 13:25:03 +0800

    update sign.sh

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-LOCAL=$HOME/repo
+LOCAL=$HOME
 
-mv $LOCAL/_repo/* $LOCAL/_repo_done;
 rsync -v -r barch:~/_repo $LOCAL&&
 (cd $LOCAL/_repo;for i in *.xz;do gpg --detach-sign $i;done)&&
 rsync -v  $LOCAL/_repo/* barch:~/_repo&&
-ssh barch "mv ~/_repo/* ~/repo/"&&
-mv $LOCAL/_repo/* $LOCAL/_repo_done/;
+ssh barch "cp ~/_repo/* ~/repo/"&&
+ssh barch "mv ~/_repo/* ~/archive/"&&
+rm -rf $LOACL/_repo/*
 #rsync --delete-after -v -r $LOCAL/_repo barch:~

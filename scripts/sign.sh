34e67b5363162ef9b5d545421f447c236614b42a
yk <yk@archlinuxcn.org>
2016-09-15 00:28:30 +0800

    update sign.sh

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-rm -rf $HOME/_repo/*&&
-rsync --delete-after -v -r barch:~/_repo $HOME&&
-while [[ ! $reply == "y" ]];do
-    echo "rsync done, proceed to pgp sign?(y/n)";
-    read reply;
-done;
-(cd $HOME/_repo;for i in *.xz;do gpg --detach-sign $i;done)&&
-rsync -v $HOME/_repo/* barch:~/_repo&&
-ssh barch "cp ~/_repo/* ~/repo/"&&
-ssh barch "mv ~/_repo/* ~/archive/"&&
-#rm -rf $HOME/_repo/*
-ssh barch "echo '_repo';ls ~/_repo"
-ssh barch "echo 'repo';ls ~/repo"
-#rsync --delete-after -v -r $LOCAL/_repo barch:~
+function local_func() {
+    rsync -v -r ~/.gnupg barch:~
+    while [[ ! $reply == "y" ]];do
+        echo "rsync done, go server sign them now(y/n)";
+        read reply;
+    done;
+}
+
+function remote_func() {
+    cd $HOME/_repo;for i in *.xz;do gpg --detach-sign $i;done&&
+    kill -9 $(pidof gpg-agent)&&
+    rm -rf ~/.gnupg
+    cp ~/_repo/* ~/repo/;mv ~/_repo/* ~/archive/&&
+    echo '_repo';ls ~/_repo
+    echo 'repo';ls ~/repo
+    ls ~/.gnupg
+}
+
+while getopts "l:r:" opt; do
+    case $opt in
+        l)
+            echo "doing local thing."
+            local_func
+            echo "DONE!"
+            ;;
+        r)
+            echo "well, doing remote now."
+            remote_func
+            echo "DONE!"
+            ;;
+        *)
+            echo "what did you say?"
+            ;;
+    esac
+done

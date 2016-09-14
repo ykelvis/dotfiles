ec1022a743f2edde4f7f27a1a26c031e7ffe23ce
yk <yk@archlinuxcn.org>
2016-09-15 00:39:43 +0800

    update sign.sh

--- scripts/sign.sh
+++ scripts/sign.sh
@@ -9,7 +9,7 @@ function local_func() {
 }
 
 function remote_func() {
-    ls $HOME/.gnupg||return 1
+    ls $HOME/.gnupg||echo "no .gnupg dir"&&return 1
     cd $HOME/_repo;for i in *.xz;do gpg --detach-sign $i;done&&
     kill -9 $(pidof gpg-agent)&&
     rm -rf ~/.gnupg
@@ -29,7 +29,7 @@ while getopts "l:r" opt; do
             ;;
         r)
             echo "well, doing remote now."
-            remote_func||echo "DONE!"||echo "error"
+            remote_func
             ;;
         *)
             echo "what did you say?"

c10a61826ccaf4e9c34f7f56b8ccea3d8947c9d8
yk <yk@archlinuxcn.org>
2016-10-14 17:42:54 +0800

    update gifmaker.sh

--- scripts/gifmaker.sh
+++ scripts/gifmaker.sh
@@ -1,12 +1,55 @@
 #!/bin/bash
 
-palette="/tmp/palette.png"
-filters="fps=25,scale=480:-1:flags=lanczos"
+usage(){
+    printf "Usage:\n-f fps\n-i input_file\n-o output_file\n-s start_time\n-t duration\n-w width\n"
+}
 
-file=$1
-out=$2
-[[ $3 != "" ]]&&ss="-ss ${3}"
-[[ $4 != "" ]]&&t="-t ${4}"
+function convertit(){
+    [[ $fps == '' ]]&&fps=20
+    [[ $width == '' ]]&&width=320
+    [[ $start_time != "" ]]&&ss="-ss ${start_time}"
+    [[ $duration != "" ]]&&t="-t ${duration}"
+    
+    palette="/tmp/palette.png"
+    filters="fps=$fps,scale=$width:-1:flags=lanczos"
+    
+    printf "#\n#\n#\n#\n#\n#\n"
+    echo running: ffmpeg $ss $t -i $input_file -vf "$filters,palettegen" -y $palette
+    ffmpeg $ss $t -i $input_file -vf "$filters,palettegen" -y $palette
+    printf "#\n#\n#\n#\n#\n#\n"
+    echo running: ffmpeg $ss $t -i $input_file -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $output_file
+    ffmpeg $ss $t -i $input_file -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $output_file
+}
 
-ffmpeg $ss $t -i $1 -vf "$filters,palettegen" -y $palette
-ffmpeg $ss $t -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
+while getopts :f:w:s:t:o:i: OPTION
+do
+    case $OPTION in
+        i)
+            input_file=${OPTARG}
+            ;;
+        o)
+            output_file=${OPTARG}
+            ;;
+        s)
+            start_time=${OPTARG}
+            ;;
+        t)
+            duration=${OPTARG}
+            ;;
+        f)
+            fps=${OPTARG}
+            ;;
+        w)
+            width=${OPTARG}
+            ;;
+        \?)
+            usage
+            exit -1
+            ;;
+    esac
+done
+if [[ $input_file == '' ]] || [[ 'output_file' == '' ]];then
+    usage
+    exit -1
+fi
+convertit

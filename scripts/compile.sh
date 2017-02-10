5285e508a0d0298ae2b40e152110e944b4dd29b5
yk <yk@archlinuxcn.org>
2017-02-10 16:23:40 +0800

    update compile.sh wont build i686

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,9 +7,9 @@ compile_it(){
     cd $1;
     echo -e "\033[31m$1 building begin\033[0m";
     archlinuxcn-x86_64-build -c -r ~/build||s1=1;
-    archlinuxcn-i686-build -c -r ~/build||s2=1;
+    #archlinuxcn-i686-build -c -r ~/build||s2=1;
     [[ ! -z $s1 ]]&&echo -e "\033[31m$1 x86_64 failed\033[0m";
-    [[ ! -z $s2 ]]&&echo -e "\033[31m$1 i686 failed\033[0m";
+    #[[ ! -z $s2 ]]&&echo -e "\033[31m$1 i686 failed\033[0m";
     mv *pkg.tar.xz ~/_repo;
     )
 }
